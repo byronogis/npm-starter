97b943c0d79b4dae80403bc107cde49c69493393
byronogis <byronogis@outlook.com>
2025-03-11 16:52:56 +0800

    test: add unit test for README.md copying functionality

--- scripts/copy-readme.mjs
+++ scripts/copy-readme.mjs
@@ -7,3 +7,17 @@ glob('README*.md').then((files) => {
     copyFile(file, `packages/pkg-placeholder/${basename(file)}`)
   })
 })
+
+if (import.meta.vitest) {
+  // const { execSync } = await import('node:child_process')
+  const { readFileSync } = await import('node:fs')
+  const { it, expect } = import.meta.vitest
+
+  it('should copy README.md correct', async () => {
+    const readme = readFileSync('README.md', 'utf8')
+    // noneed this because it's already run while testing since file defined in test.includeSource
+    // execSync('node scripts/copy-readme.mjs')
+    const copiedReadme = readFileSync('packages/pkg-placeholder/README.md', 'utf8')
+    expect(copiedReadme).toBe(readme)
+  })
+}
