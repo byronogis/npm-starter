0969df56afa34ec462bcf6ede012d91272bc793c
byronogis <byronogis@outlook.com>
2025-03-11 16:52:40 +0800

    test: update vitest configuration to exclude playground and coverage directories

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vitest/config'
+import { coverageConfigDefaults, defaultExclude, defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    exclude: [
+      'playground/**',
+      ...defaultExclude,
+    ],
     coverage: {
       enabled: true,
-      include: ['src'],
+      exclude: [
+        '**/dist/**',
+        '**/coverage/**',
+        'playground/**',
+        ...coverageConfigDefaults.exclude,
+      ],
     },
   },
 })
