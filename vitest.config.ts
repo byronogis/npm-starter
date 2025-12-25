c3c2cde3b987559eccc7a25297ee17b6342bfb07
byronogis <byronogis@outlook.com>
2025-12-25 11:18:03 +0800

    test: exports

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,10 +3,16 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     projects: [
+      'test',
       'packages/*',
     ],
     coverage: {
       enabled: true,
+    },
+    server: {
+      deps: {
+        inline: ['vitest-package-exports'],
+      },
     },
   },
 })
